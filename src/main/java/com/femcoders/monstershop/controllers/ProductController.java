6eee932fca90a6993936ff867e369df710412606
Lara Pla <laraplamoreno@gmail.com>
2025-06-17 01:58:20 +0200

    feat(Product, ProductRequest): add empty constructor, add addProduct methods and fix NotBlank in price (double)

--- src/main/java/com/femcoders/monstershop/controllers/ProductController.java
+++ src/main/java/com/femcoders/monstershop/controllers/ProductController.java
@@ -1,11 +1,15 @@
 package com.femcoders.monstershop.controllers;
 
+import com.femcoders.monstershop.dtos.ProductRequest;
 import com.femcoders.monstershop.dtos.ProductResponse;
 import com.femcoders.monstershop.models.Product;
 import com.femcoders.monstershop.services.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +25,10 @@ public class ProductController {
     @GetMapping("/api/products")
     public ResponseEntity<List<ProductResponse>> getAllProducts() {
         return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+    }
+
+    @PostMapping("/api/products")
+    public ResponseEntity<ProductResponse> addEvent(@Valid @RequestBody ProductRequest productRequest) {
+        return new ResponseEntity<>(productService.addProduct(productRequest), HttpStatus.CREATED);
     }
 }
--- src/main/java/com/femcoders/monstershop/dtos/ProductRequest.java
+++ src/main/java/com/femcoders/monstershop/dtos/ProductRequest.java
@@ -1,13 +1,14 @@
 package com.femcoders.monstershop.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record ProductRequest(
         @NotBlank(message = "Name is required")
         @Size(min = 2, max = 50, message = "Name must contain min 2 and max 50 characters")
         String name,
-        @NotBlank(message = "Price is required")
+        @NotNull(message = "Price is required")
         double price,
         @NotBlank(message = "Image URL is required")
         String imageUrl,
