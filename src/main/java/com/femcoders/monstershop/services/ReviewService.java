8efffefd7872990460d69a0ffe863b1976d7ab45
Lara Pla <laraplamoreno@gmail.com>
2025-06-17 21:53:31 +0200

    feat(ReviewService, ReviewController): add getReviewsByProductId methods

--- src/main/java/com/femcoders/monstershop/services/ReviewService.java
+++ src/main/java/com/femcoders/monstershop/services/ReviewService.java
@@ -1,13 +1,28 @@
 package com.femcoders.monstershop.services;
 
+import com.femcoders.monstershop.dtos.review.ReviewMapper;
+import com.femcoders.monstershop.dtos.review.ReviewResponse;
+import com.femcoders.monstershop.models.Product;
+import com.femcoders.monstershop.models.Review;
+import com.femcoders.monstershop.repositories.ProductRepository;
 import com.femcoders.monstershop.repositories.ReviewRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class ReviewService {
     private final ReviewRepository reviewRepository;
+    private final ProductRepository productRepository;
 
-    public ReviewService(ReviewRepository reviewRepository) {
+    public ReviewService(ReviewRepository reviewRepository, ProductRepository productRepository) {
         this.reviewRepository = reviewRepository;
+        this.productRepository = productRepository;
+    }
+
+    public List<ReviewResponse> getReviewsByProductId(Long productId) {
+        Product product = productRepository.findById(productId).orElseThrow(() -> new NoSuchElementException("Product not found"));
+        return product.getReviews().stream().map(review -> ReviewMapper.entityToDto(review)).toList();
     }
 }
