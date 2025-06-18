9e65309d8e6b704c5536468dd2a806b2ae5a5b45
Lara Pla <laraplamoreno@gmail.com>
2025-06-18 02:17:51 +0200

    feat(ReviewService, ReviewController): add addReview methods

--- src/main/java/com/femcoders/monstershop/services/ReviewService.java
+++ src/main/java/com/femcoders/monstershop/services/ReviewService.java
@@ -1,6 +1,7 @@
 package com.femcoders.monstershop.services;
 
 import com.femcoders.monstershop.dtos.review.ReviewMapper;
+import com.femcoders.monstershop.dtos.review.ReviewRequest;
 import com.femcoders.monstershop.dtos.review.ReviewResponse;
 import com.femcoders.monstershop.models.Product;
 import com.femcoders.monstershop.models.Review;
@@ -24,5 +25,13 @@ public class ReviewService {
     public List<ReviewResponse> getReviewsByProductId(Long productId) {
         Product product = productRepository.findById(productId).orElseThrow(() -> new NoSuchElementException("Product not found"));
         return product.getReviews().stream().map(review -> ReviewMapper.entityToDto(review)).toList();
+    }
+
+    public ReviewResponse addReview(Long productId, ReviewRequest reviewRequest) {
+        Product product = productRepository.findById(productId).orElseThrow(() -> new NoSuchElementException("Product not found"));
+        Review newreview = ReviewMapper.dtoToEntity(reviewRequest);
+        newreview.setProduct(product);
+        Review savedReview = reviewRepository.save(newreview);
+        return ReviewMapper.entityToDto(savedReview);
     }
 }
